07394f5fd9007dc2d448e3dd6e3e5eb38a97ce69
JeonSuna <jsa020910@gmail.com>
2024-10-15 17:29:30 +0900

    prettier 및 추가적 기초 설정 완료

---
+++ test.js
@@ -0,0 +1,13 @@
+const a = 100;
+console.log('Hello World')
+function a() {
+    console.log('Hello')
+}
+
+
+function a() {
+    console.log('Hello')
+    if (a === 100) {
+        console.log(100)
+    }
+}
