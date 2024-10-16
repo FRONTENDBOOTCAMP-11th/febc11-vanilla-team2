44217a17828ce8316af60385335202940d49e1fd
JeonSuna <jsa020910@gmail.com>
2024-10-16 15:52:26 +0900

    💄 style: login-css 추가

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 index: resolve(__dirname, 'index.html'), // 기본 index.html
-                // login: resolve(__dirname, 'src/pages/auth/login.html'), // 추가 HTML 파일
+                login: resolve(__dirname, 'src/login/login.html'), // 추가 HTML 파일
                 // list: resolve(__dirname, 'src/pages/board/list.html'), // 추가 HTML 파일
                 // 필요한 다른 HTML 파일을 여기에 추가
             },
