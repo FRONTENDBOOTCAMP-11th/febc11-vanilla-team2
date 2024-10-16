6e875ece6d2be9106754bf4c67bf6e17a856c6a3
khk99 <khk990815@gmail.com>
2024-10-16 16:50:46 +0900

    🎉헤더 로고 및 버튼 삽입

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 // login: resolve(__dirname, 'src/pages/auth/login.html'), // 추가 HTML 파일
                 // list: resolve(__dirname, 'src/pages/board/list.html'), // 추가 HTML 파일
                 // 필요한 다른 HTML 파일을 여기에 추가
+                main: resolve(__dirname, 'src/main.html'),
             },
         },
     },
