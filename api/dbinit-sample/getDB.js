b5d3d4d74de9348f3885f41171cdcb5373642a4b
JeonSuna <jsa020910@gmail.com>
2024-10-23 09:34:27 +0900

    ✨ feat : db초기화 완료

--- api/dbinit-sample/getDB.js
+++ api/dbinit-sample/getDB.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 async function getDB(clientId) {
   // const url = `${process.env.DB_URL}/${clientId}?authSource=admin`;
@@ -11,16 +11,16 @@ async function getDB(clientId) {
 
     await client.connect();
     const db = client.db(clientId);
-    db.user = db.collection('user');
-    db.product = db.collection('product');
-    db.cart = db.collection('cart');
-    db.order = db.collection('order');
-    db.review = db.collection('review');
-    db.seq = db.collection('seq');
-    db.code = db.collection('code');
-    db.bookmark = db.collection('bookmark');
-    db.config = db.collection('config');
-    db.post = db.collection('post');
+    db.user = db.collection("user");
+    db.product = db.collection("product");
+    db.cart = db.collection("cart");
+    db.order = db.collection("order");
+    db.review = db.collection("review");
+    db.seq = db.collection("seq");
+    db.code = db.collection("code");
+    db.bookmark = db.collection("bookmark");
+    db.config = db.collection("config");
+    db.post = db.collection("post");
 
     const nextSeq = async _id => {
       let result = await db.seq.findOneAndUpdate({ _id }, { $inc: { no: 1 } });
@@ -29,7 +29,7 @@ async function getDB(clientId) {
         await db.seq.insertOne({ _id, no: 2 });
       }
       return result.no;
-    }
+    };
 
     return { db, client, nextSeq };
   } catch (err) {
