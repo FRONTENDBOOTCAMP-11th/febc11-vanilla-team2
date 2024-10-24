062833724003bcbb36c37dbfe9b0582290377bed
Ansinnn <siun2009@gmail.com>
2024-10-24 11:18:44 +0900

    ✨ feat : 취소버튼 클릭시 main페이지 이동

--- src/pages/write/write.js
+++ src/pages/write/write.js
@@ -1,21 +1,22 @@
 import axios from "axios";
 
 document.getElementById("btnPost").addEventListener("click", postClick);
+document.getElementById("btnExit").addEventListener("click", exitClick);
 
 async function postClick() {
   let mainTitle = document.querySelector("#mainTitle");
   let subTitle = document.querySelector("#subTitle");
-  let contents = document.querySelector("#contents");
+  let content = document.querySelector("#contents");
 
-  if (!mainTitle || !contents) {
+  if (!mainTitle || !content) {
     alert("제목 혹은 내용을 입력해주세요.");
     return;
   }
 
   const postData = {
-    mainTitle: mainTitle,
-    subTitle: subTitle,
-    contents: contents,
+    mainTitle: mainTitle.value,
+    subTitle: subTitle.value,
+    content: content.vaule,
   };
 
   try {
@@ -25,10 +26,24 @@ async function postClick() {
         "client-id": "vanilla02",
       },
     });
+
+    console.log(response);
+    // 객체 생성 후 입력칸 초기화
+    mainTitle.value = "";
+    subTitle.value = "";
+    content.value = "";
+
     console.log("Post Success! :", response.data);
     alert("게시글이 성공적으로 등록되었습니다");
   } catch (error) {
     console.error("Error Post! :", error);
     alert("게시글 등록에 실패했습니다.");
+  }
+}
+
+function exitClick() {
+  const exit = confirm("글쓰기를 취소하시겠습니까?");
+  if (exit) {
+    window.open("main.html");
   }
 }
