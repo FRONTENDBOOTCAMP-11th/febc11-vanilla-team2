c5c1cb6ef87baf2630111c68aec2a5a8fdceb082
khk99 <khk990815@gmail.com>
2024-10-29 10:31:08 +0900

    add

--- src/components/header.js
+++ src/components/header.js
@@ -7,6 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
     })
     .then(data => {
       document.querySelector("header").innerHTML = data;
+
+      // 세션 스토리지에서 accessToken 확인
+      const accessToken = sessionStorage.getItem("accessToken");
+      if (accessToken) {
+        console.log("세션 스토리지에 accessToken이 존재합니다:", accessToken);
+      } else {
+        console.log("세션 스토리지에 accessToken이 없습니다.");
+      }
     })
     .catch(err => console.error("Failed to load header.html", err));
 });
