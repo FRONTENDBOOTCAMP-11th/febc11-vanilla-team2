ce7447d122d415f37823208f479fa724a2231f15
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-10-30 14:14:44 +0900

    ✨ feat : 내 브런치 기능 완료

--- src/pages/myDrawer/myDrawer.js
+++ src/pages/myDrawer/myDrawer.js
@@ -1,9 +1,88 @@
 import axios from "axios";
 
-const favAuthorsList = document.querySelector(".fav-authors__list");
+const apiUrl = "https://11.fesp.shop";
+const clientId = "vanilla02";
 
-// const getFavAuthorList = async () => {
-//   try {
+const accessToken = sessionStorage.getItem("accessToken");
 
-//   } catch (error) {}
-// };
+// 로그인 체크 및 페이지 렌더링
+window.addEventListener("load", async () => {
+  if (!accessToken) {
+    alert("로그인이 필요합니다. 로그인 페이지로 이동합니다.");
+    window.location.href = "/src/pages/login/login.html";
+  } else {
+    await rednerMyBrunch();
+  }
+});
+
+// DOM Node
+const $favAuthorList = document.querySelector(".fav-author__list");
+const $recentViewList = document.querySelector(".recent-view__list");
+const $favArticleList = document.querySelector(".fav-article__list");
+const $myBrunchList = document.querySelector(".my-brunch__lists");
+
+// Render Function
+// 관심 작가
+const rednerFavAuthor = async () => {};
+
+// 내 브런치
+const rednerMyBrunch = async () => {
+  try {
+    const response = await axios.get(`${apiUrl}/posts/users`, {
+      headers: {
+        "Content-Type": "application/json",
+        "client-id": clientId,
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const myBrunches = response.data.item;
+    console.log(myBrunches);
+    $myBrunchList.innerHTML = myBrunches
+      .map(myBrunch => {
+        return `
+        <li class="my-brunch__list">
+          <h3 class="my-brunch__subTitle">${myBrunch.subTitle}</h3>
+          <p class="my-brunch__title">${myBrunch.title}</p>
+          <p class="my-brunch__createdAt">${formatDate(myBrunch.createdAt)}</p>
+        </li>
+      `;
+      })
+      .join("");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Callback Function
+// 관심 작가 페이지 이동
+const handleFavAuthor = () => {};
+
+// 최근 본 브런치 상세페이지로 이동
+const handleRecentView = () => {};
+
+// 관심 글 브런치 상세페이지로 이동
+const handleFavArticle = () => {};
+
+// 내 브런치 상세페이지로 이동
+const handleMyBrunch = async () => {};
+
+// Event Listener
+// 관심 작가 클릭 시 작가 페이지로 이동
+$favAuthorList.addEventListener("click", handleFavAuthor);
+
+// 최근 본 브런치 클릭 시 상세페이지로 이동
+$recentViewList.addEventListener("click", handleRecentView);
+
+// 관심 글 브런치 클릭 시 상세페이지로 이동
+$favArticleList.addEventListener("click", handleFavArticle);
+
+// 내 브런치 클릭 시 상세페이지로 이동
+$myBrunchList.addEventListener("click", handleMyBrunch);
+
+// Formatting Function
+// 날짜 형식 변환 함수
+const formatDate = createdAt => {
+  const date = new Date(createdAt);
+  const options = { year: "numeric", month: "short", day: "numeric" };
+  return date.toLocaleDateString("en-US", options).replace(",", " /");
+};
